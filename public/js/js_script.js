1f97d53c0e3cbdaa20b90421047814aa15dab4a2
firepringles <joachimforsberg788@gmail.com>
2020-02-05 00:24:41 +0100

    Button working

--- public/js/js_script.js
+++ public/js/js_script.js
@@ -1,40 +1,10 @@
-function menuItem(name, kCal, gluten, lactos, image){
-  this.name = name;
-  this.kCal = kCal;
-  this.gluten = gluten;
-  this.lactos = lactos;
-  this.image = image;
-  this.info = function(){
-    if(this.gluten == true && this.lactos == true){
-    	return this.kCal + " Lactos" + " Gluten";
-      }
-    else if(this.gluten == true){
-    	return this.kCal + " Gluten";
-    }
-        else if(this.lactos == true){
-    	return this.kCal + " Lactos";
-    }
-    else {
-    return this.kCal;
-    }
-  };
-}
-
-let item1 = new menuItem("Ultimate Burger","10000kCal",true,false,"https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/pass/the-ultimate-hamburger.jpg");
-let item2 = new menuItem("Pale Burger","20000kCal",true,true,"https://assets.biggreenegg.eu/app/uploads/2019/03/28145521/topimage-classic-hamburger-2019m04-800x534.jpg");
-let item3 = new menuItem("Tripple Wombo Combo","30000kCal",false,false,"https://assets.biggreenegg.eu/app/uploads/2017/12/05133526/topimage-mango-filled-hamburgers-800x500-600x375.jpg");
-let item4 = new menuItem("Saucy Burger","40000kCal",false,false,"https://mb.cision.com/Public/12521/2851454/aa42a4d1cb9c6ca5_800x800ar.jpg");
-let item5 = new menuItem("Bacon Burger","50000kCal",false,true,"https://i2.wp.com/media.victoriasprovkok.se/2017/08/image-139.jpeg");
-let item6 = new menuItem("BBQ Burger","60000kCal",true,true,"https://www.javligtgott.se/wp-content/uploads/2019/07/IMG_0557-1024x682.jpg");
-
-let menu = [item1, item2, item3, item4, item5, item6];
-
+/*
 let myElement = document.getElementById("myID");
 let boxName = "A";
 function nextCharacter(c) {
   return String.fromCharCode(c.charCodeAt(0) + 1);
 }
-for (index in menu)
+for (index in food)
 {
         let divItem = document.createElement("div");
         divItem.setAttribute('class', boxName);
@@ -59,3 +29,29 @@ for (index in menu)
         myElement.appendChild(divItem);
         boxName = nextCharacter(boxName);
 }
+*/
+
+
+/*
+let orderButton = document.getElementById('orderButton');
+orderButton.addEventListener("click", triggerFunc);
+function triggerFunc(){
+  let x = document.getElementById('fullname').value;
+  let y = document.getElementById('email').value;
+  let z = document.getElementById('street').value;
+  let v = document.getElementById('housenumb').value;
+  let w = document.getElementById('recipient').value;
+  var ele = document.getElementsByName('gender');
+
+for(i = 0; i < ele.length; i++) {
+    if(ele[i].checked)
+    ele = ele[i].value;
+}
+  console.log(x);
+  console.log(y);
+  console.log(z);
+  console.log(v);
+  console.log(w);
+  console.log(ele);
+}
+*/
--- public/js/vue_script.js
+++ public/js/vue_script.js
@@ -1,34 +1,27 @@
-function menuItem(name, kCal, gluten, lactos, image){
-  this.name = name;
-  this.kCal = kCal;
-  this.gluten = gluten;
-  this.lactos = lactos;
-  this.image = image;
-  this.info = function(){
-    if(this.gluten == true && this.lactos == true){
-    	return this.kCal + " Lactos" + " Gluten";
-      }
-    else if(this.gluten == true){
-    	return this.kCal + " Gluten";
-    }
-        else if(this.lactos == true){
-    	return this.kCal + " Lactos";
-    }
-    else {
-    return this.kCal;
-    }
-  };
-}
-let item1 = new menuItem("Ultimate Burger","10000kCal",true,false,"https://assets.epicurious.com/photos/57c5c6d9cf9e9ad43de2d96e/master/pass/the-ultimate-hamburger.jpg");
-let item2 = new menuItem("Pale Burger","20000kCal",true,true,"https://assets.biggreenegg.eu/app/uploads/2019/03/28145521/topimage-classic-hamburger-2019m04-800x534.jpg");
-let item3 = new menuItem("Tripple Wombo Combo","30000kCal",false,false,"https://assets.biggreenegg.eu/app/uploads/2017/12/05133526/topimage-mango-filled-hamburgers-800x500-600x375.jpg");
-let item4 = new menuItem("Saucy Burger","40000kCal",false,false,"https://mb.cision.com/Public/12521/2851454/aa42a4d1cb9c6ca5_800x800ar.jpg");
-let item5 = new menuItem("Bacon Burger","50000kCal",false,true,"https://i2.wp.com/media.victoriasprovkok.se/2017/08/image-139.jpeg");
-let item6 = new menuItem("BBQ Burger","60000kCal",true,true,"https://www.javligtgott.se/wp-content/uploads/2019/07/IMG_0557-1024x682.jpg");
-
 const vm = new Vue({
   el: '#myID',
   data: {
-    menu: [item1, item2, item3, item4, item5, item6]
+    menu: food,
+    burger: "",
+    name: "",
+    email: "",
+    street: "",
+    housenumb: "",
+    payment: "Credit card",
+    gender: "male",
+    seen: false
   },
+  methods: {
+    markDone: function(){
+      this.seen = true;
+      alert("Type of burger: " + this.burger + '\n' +
+       "Customer name: " + this.name + '\n' +
+       "Customer email: " + this.email + '\n' +
+       "Customer adress: " + this.street + '\n' +
+       "Customer house number: " + this.housenumb + '\n' +
+       "Customer payment option: " + this.payment + '\n' +
+       "Customer gender: " + this.gender)
+    }
+  }
+
 })
